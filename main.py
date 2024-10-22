09faaf850472221f8bdd4cf858af69cf8e077c24
LucyFair <anubisereje@gmail.com>
2024-10-22 17:18:48 +0300

    Adatbázis lokal host helyett egy rasberry pi-n fut így mindhárman hozzáférünk

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from Users import User
+import Reserves
 from Positions import Position
+from Users import Users
 from Rooms import Room
 from Reserves import Reserve
+
 
 # --- Define the Base for Models ---
 Base = sqlalchemy.orm.declarative_base();
@@ -29,8 +31,12 @@ def create_database(db_url):
 
 
 if __name__ == "__main__":
-    database_url = 'mysql+pymysql://root:Almafa%401@localhost/RoomReserver'
+    database_url = 'mysql+pymysql://borbmaty:Almafa2@192.168.172.207:3306/ProjectDatabase'
+
     session = get_session(database_url)
-    # User.addUser(session,"Borbath Matyas", "borbath.matyas@student.ms.sapientia.ro", 100, 3)
+    create_database(database_url)
+    Users.addUser(session,"Borbath Matyas", "borbath.matyas@student.ms.sapientia.ro", 100, 3)
     #User.addUser(session, "Korpos Botond", "korpos.botond@student.ms.sapientia.ro", 100, 3)
-    Room.addRoom(session,230)
+    #Room.addRoom(session,230)
+    #Position.addPosition(session,"Tanar",200)
+    #Reserves.addReserve(session,1,230,'2024-10-22','9:00:00','11:00:00')
