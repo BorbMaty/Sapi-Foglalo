da301d126d9127770a780edf8a21043f05292015
LucyFair <anubisereje@gmail.com>
2024-10-28 18:35:10 +0200

    Added necesarry dal functions

--- routes/Reserves.py
+++ routes/Reserves.py
@@ -24,7 +24,60 @@ class Reserve(Base):
         self.StartHour = StartHour
         self.EndHour = EndHour
 
-    def addReserve(session, UserId, RoomId, Date, StartHour, EndHour):
-        new_reserve = Reserve(UserId=UserId, RoomId=RoomId, Date=Date, StartHour=StartHour, EndHour=EndHour)
-        session.add(new_reserve)
-        session.commit()
+
+class ReserveDAL:
+    def __init__(self, session):
+        self.session = session
+
+    def addReserve(self, UserId, RoomId, Date, StartHour, EndHour):
+        new_reserve = Reserve(UserId, RoomId, Date, StartHour, EndHour)
+        self.session.add(new_reserve)
+        self.session.commit()
+        return new_reserve
+
+    def getReserve(self, ReserveId):
+        return self.session.query(Reserve).filter(Reserve.ReserveId == ReserveId).first()
+
+    def getAllReserves(self):
+        return self.session.query(Reserve).all()
+
+    def updateReserve(self, ReserveId, UserId=None, RoomId=None, Date=None, StartHour=None, EndHour=None):
+        reserve = self.get_reserve(ReserveId)
+        if reserve:
+            if UserId is not None:
+                reserve.UserId = UserId
+            if RoomId is not None:
+                reserve.RoomId = RoomId
+            if Date is not None:
+                reserve.Date = Date
+            if StartHour is not None:
+                reserve.StartHour = StartHour
+            if EndHour is not None:
+                reserve.EndHour = EndHour
+            self.session.commit()
+        return reserve
+
+    def deleteReserve(self, ReserveId):
+        reserve = self.get_reserve(ReserveId)
+        if reserve:
+            self.session.delete(reserve)
+            self.session.commit()
+        return reserve
+
+# Example usage
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+# engine = create_engine('your_database_url')
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# reserve_dal = ReserveDAL(session)
+# reserve_dal.add_reserve(UserId=1, RoomId=2, Date='2024-10-30', StartHour='09:00:00', EndHour='10:00:00')
+# reserve = reserve_dal.get_reserve(1)
+# print(reserve)
+# all_reserves = reserve_dal.get_all_reserves()
+# print(all_reserves)
+# reserve_dal.update_reserve(1, EndHour='11:00:00')
+# reserve_dal.delete_reserve(1)
+# session.close()
