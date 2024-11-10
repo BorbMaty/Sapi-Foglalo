b418ab9cfccea3b45d1325562ac4ec71827e97d9
ElekesKolloJeno <125959296+ElekesKolloJeno@users.noreply.github.com>
2024-11-10 13:14:09 +0200

    updating the first floor selector

--- static/script.js
+++ static/script.js
@@ -13,10 +13,12 @@ function closeModal() {
 function submitBooking() {
     const name = document.getElementById('name').value;
     const date = document.getElementById('date').value;
+    const start_hour = document.getElementById('start_hour').value;
+    const end_hour = document.getElementById('end_hour').value;
     const room = document.getElementById('roomName').textContent;
     
     if (name && date) {
-        alert(`Room "${room}" booked successfully by ${name} on ${date}!`);
+        alert(`Room "${room}" booked successfully by ${name} on ${date} from ${start_hour} to ${end_hour}!`);
         closeModal();
     } else {
         alert("Please fill in all the details.");
